37b7c6dacfee232ce3e723a2231768a9745ae2b4
Ryley McConkey <ryley@pop-os.localdomain>
2022-06-02 20:25:55 -0400

    added coefficient based error

--- utilities/foam_automation/error.py
+++ utilities/foam_automation/error.py
@@ -46,7 +46,28 @@ def mseU_2Dmag(foamdirectory = os.getcwd(),
     mse = mean_squared_error(U_ref_mag,U_sim_mapped_mag)
     return mse
 
-    
+def mseU_2Dmag_relcoeff(foamdirectory = os.getcwd(),
+                        foamtime = None,
+                        ref_df = None,
+                        interp_method='linear',
+                        coef = None,
+                        coef_default = None
+                       ):
+    foamdirectory = os.path.abspath(foamdirectory)
+    U_sim = Ofpp.parse_internal_field(os.path.join(foamdirectory,foamtime,'U'))[:,0:2]
+    points_sim = get_cell_centres(foamdirectory)[:,0:2]
+    U_sim_mapped = map_to_reference(U_sim,points_sim,ref_df[['x','y']].values,method=interp_method)             
+    U_ref = ref_df[['Ux','Uy']].values
+    U_sim_mapped_mag = np.linalg.norm(U_sim_mapped,axis=1)
+    U_ref_mag = np.linalg.norm(U_ref,axis=1)
+    print(U_ref_mag.shape)
+    print(U_ref_mag[0])
+    print(U_ref[0,:])
+    mse = mean_squared_error(U_ref_mag,U_sim_mapped_mag)
+    rel_coef = (coef-coef_default)/coef_default
+    score = mse + rel_coef
+    return score
+
 def foam_mse(ref_df):
     U_sim = Ofpp.parse_internal_field('foam/case_1p0/5000/U')[:,0:2]
     mse = mean_squared_error(ref_df[['Ux','Uy']].values,U_sim)
