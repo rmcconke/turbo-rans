0a4d8440151ccf1a2c728ef09bbb95df200e3224
rmcconke <rmcconke@uwaterloo.ca>
2022-06-02 20:01:07 -0400

    update

--- utilities/foam_automation/error.py
+++ utilities/foam_automation/error.py
@@ -3,6 +3,7 @@ import openfoamparser as Ofpp
 from scipy.interpolate import griddata
 #from dataFoam.utilities.foamIO.readFoam import get_cell_centres
 from sklearn.metrics import mean_squared_error
+import numpy as np
 
 def get_cell_centres(foamdirectory):
     if not os.path.exists(os.path.join(foamdirectory,'0','C')):
@@ -26,6 +27,23 @@ def mseU_2Dxy(foamdirectory = os.getcwd(),
     U_sim_mapped = map_to_reference(U_sim,points_sim,ref_df[['x','y']].values,method=interp_method)                           
     U_ref = ref_df[['Ux','Uy']].values
     mse = mean_squared_error(U_ref,U_sim_mapped)
+    return mse
+
+def mseU_2Dmag(foamdirectory = os.getcwd(),
+             foamtime = None,
+             ref_df = None,
+             interp_method='linear'):
+    foamdirectory = os.path.abspath(foamdirectory)
+    U_sim = Ofpp.parse_internal_field(os.path.join(foamdirectory,foamtime,'U'))[:,0:2]
+    points_sim = get_cell_centres(foamdirectory)[:,0:2]
+    U_sim_mapped = map_to_reference(U_sim,points_sim,ref_df[['x','y']].values,method=interp_method)             
+    U_ref = ref_df[['Ux','Uy']].values
+    U_sim_mapped_mag = np.linalg.norm(U_sim_mapped,axis=1)
+    U_ref_mag = np.linalg.norm(U_ref,axis=1)
+    print(U_ref_mag.shape)
+    print(U_ref_mag[0])
+    print(U_ref[0,:])
+    mse = mean_squared_error(U_ref_mag,U_sim_mapped_mag)
     return mse
 
     
