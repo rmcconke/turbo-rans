e9643dad167c9cd727febf61400006a211e546e5
rmcconke <rmcconke@uwaterloo.ca>
2023-10-17 11:51:16 -0400

    example cleanup, periodic hills example running

--- examples/openfoam_periodichills/periodichills_example.sh
+++ examples/openfoam_periodichills/periodichills_example.sh
@@ -4,6 +4,5 @@ export PYTHONPATH=../../
 ./reset_example.sh
 cp -r periodichills_template periodichills_running
 # Optionally move tuner directory outside of running directory
-mv periodichills_running/periodichills_tuner periodichills_tuner
-rm -r periodichills_running/tuner
-python3 periodichills_turbo.py | tee example.log
+#mv periodichills_running/periodichills_tuner periodichills_tuner
+python3 -u periodichills_turbo.py | tee example.log
--- examples/openfoam_periodichills/reset_example.sh
+++ examples/openfoam_periodichills/reset_example.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo Resetting periodichills example....
-rm -r example.log periodichills_running periodichills_opt periodichills_tuner
+rm -r periodichills_running
+rm -r periodichills_opt
+rm -r periodichills_tuner/turborans_history_sorted.csv periodichills_tuner/turborans_target_history.png
+rm -r example.log
 
 
