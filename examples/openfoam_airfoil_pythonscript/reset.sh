1865d1265957ec6fbce492e88662f4247780aeed
rmcconke <rmcconke@uwaterloo.ca>
2023-11-26 21:51:13 -0500

    fixed airfoil reset script

--- examples/openfoam_airfoil_pythonscript/reset.sh
+++ examples/openfoam_airfoil_pythonscript/reset.sh
@@ -4,6 +4,5 @@ rm -r airfoil_running
 rm -r airfoil_opt
 rm -r airfoil_tuner/turborans_history_sorted.csv airfoil_tuner/turborans_target_history.png airfoil_tuner/settings.json airfoil_tuner/history.json airfoil_tuner/suggestion.json airfoil_tuner/mode.json
 rm -r example.log
-cp -r airfoil_template airfoil_running
 
 
